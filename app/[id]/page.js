e36e3a43b7f90708671a16a5be80308cfe5fa6f2
whenceIT <whenceit@gmail.com>
2026-03-17 17:26:11 +0200

    fixed employee id issue

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -4,7 +4,10 @@ import EmployeeProfileClient from "../components/employeeprofile"
 export default async function Page(props) {
   // Unwrap the params using await
   const { id } = await props.params
+  function getNumberAfterDash(str) {
+  return str.split('-').pop();
+}
 
   // Pass ID to the client component
-  return <EmployeeProfileClient id={id} />
+  return <EmployeeProfileClient id={getNumberAfterDash(id)} />
 }
